b6f6c3e826f25ad7ab118227779670c97b29d0ef
Tosin5S <olyguy.aq@gmail.com>
2021-09-26 19:46:13 +0100

    Add serializers

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # All-My-Django-Projects
-This repo is for practise and personal improvement while learning Django and Git
+This repo is for practice and personal improvement while learning Django and Git
--- blogapi/blog_project/settings.py
+++ blogapi/blog_project/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd-party apps
+    'rest_framework',
+
     # Local
     'posts.apps.PostsConfig',
 ]
@@ -127,3 +130,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.AllowAny',
+    ]
+}
