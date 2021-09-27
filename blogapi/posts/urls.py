1bbab5c6f667f86a6973b6adaf8f7eeb3f2e3d1e
Tosin5S <olyguy.aq@gmail.com>
2021-09-27 11:44:34 +0100

    Add Routers

--- blogapi/posts/urls.py
+++ blogapi/posts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from .views import UserList, UserDetail, PostList, PostDetail
+from .views import UserViewSet, PostViewSet
 
-urlpatterns = [
-    path('users/', UserList.as_view()),
-    path('users/<int:pk>/', UserDetail.as_view()),
-    path('<int:pk>/', PostDetail.as_view()),
-    path('', PostList.as_view()),
-]
+router = SimpleRouter()
+router.register('users', UserViewSet, base_name='users')
+router.register('', PostViewSet, base_name='posts')
+
+urlpatterns = router.urls
