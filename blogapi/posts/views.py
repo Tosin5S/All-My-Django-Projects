6dc967d653f12a8481e1ea941f94c2cc51985556
Tosin5S <olyguy.aq@gmail.com>
2021-09-27 11:27:09 +0100

    Add User List and User Details

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 from rest_framework import generics
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 
 # Create your views here.
 
@@ -15,3 +16,11 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
