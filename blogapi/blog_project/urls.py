1b499ed3d67c7942710633eb97b3c32d7ebb80f1
Tosin5S <olyguy.aq@gmail.com>
2021-09-27 22:20:51 +0100

    Add Documentation

--- blogapi/blog_project/urls.py
+++ blogapi/blog_project/urls.py
@@ -15,6 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
+
+API_TITLE = 'Blog API'
+API_DESCRIPTION = 'A Web API for creating and editing blog post.'
+schema_view = get_schema_view(title=API_TITLE)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +28,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/rest-auth/', include('rest_auth.urls')),
     path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
+    path('schema/', schema_view),
 ]
