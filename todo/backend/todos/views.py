6972c1abd9381ec9ca39a1de782fdecd876d883a
Tosin5S <olyguy.aq@gmail.com>
2021-09-28 15:32:24 +0100

    Update Todo

--- todo/backend/todos/views.py
+++ todo/backend/todos/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Todo
+from .serializers import TodoSerializer
 
 # Create your views here.
+
+class ListTodo(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
